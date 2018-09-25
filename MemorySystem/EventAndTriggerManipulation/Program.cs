4072c4919664fdd4a61c249d4472b51ef86b98cd
Coombszy <LCCoombs@hotmail.co.uk>
2018-09-25 15:22:03 +0100

    EventAndTriggerManipulation => MemorySystem. MemorySystem successfully loads all events from xml into node structure with basic navigation system

---
+++ MemorySystem/EventAndTriggerManipulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EventAndTriggerManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryHandler Test = new MemoryHandler();
+            Test.BuildEventList();
+            Test.BuildMemoryStructure();
+            string text;
+            while (true)
+            {
+                Console.WriteLine("ENTER TEXT");
+                text = Console.ReadLine();
+                if (Test.GetEventsofActiveNode().Contains(text))
+                {
+                    Test.Navigate(text);
+                    Console.WriteLine(Test.GetTrigger());
+                }
+            }
+            Console.WriteLine("END!");
+            Console.ReadLine();
+        }
+    }
+}
