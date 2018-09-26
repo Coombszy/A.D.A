83e8936189e2af833611e19ace36ea22723c5ec7
Coombszy <LCCoombs@hotmail.co.uk>
2018-09-26 11:50:52 +0100

    UserInterpreter Started! - Will operate using console until Voice Recog Completion

---
+++ A.D.A Host/A.D.A Host/UserInterpreter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.D.A_Host
+{
+    class UserInterpreter
+    {
+        private MemoryHandler MemoryUnit;
+        private List<string> EventDicitionary;
+        public UserInterpreter(MemoryHandler activeMemoryHandler)
+        {
+            this.MemoryUnit = activeMemoryHandler;
+        }
+        public void buildEventDictionary()
+        {
+            
+        }
+    }
+}
